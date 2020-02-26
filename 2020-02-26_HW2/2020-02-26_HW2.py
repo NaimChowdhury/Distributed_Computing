50c2798aa5eb06dcf0b04fd5b1c94d377b2856fa
NaeemCho <naimul.chowdhury15@ncf.edu>
2020-02-26 15:49:22 -0500

    Added mapreduces to compute names that are gender neutral

--- 2020-02-26_HW2/2020-02-26_HW2.py
+++ 2020-02-26_HW2/2020-02-26_HW2.py
@@ -6,7 +6,7 @@ useless = sc.parallelize(range(1000000))
 
 def mapping(x):
     row = [rand.randint(1,6) for i in range(1,8)]
-    if sum( row[0:5] ) > sum( row[5:8] ):
+    if sum( row[0:4] ) > sum( row[4:7] ):
         return (1,1)
     else:
         return (0,1)
@@ -21,6 +21,8 @@ count = useful.reduceByKey(add)
 probability = count.collect()[1][1] / (count.collect()[0][1] + count.collect()[1][1])
 
 # Problem B
+
+# If we don't care about the ethnicity, how can we use spark to remove repetition?
 set1 = sc.textFile("/usr/examples/Popular_Baby_Names.csv")
 
 set1header = set1.first()
@@ -29,14 +31,14 @@ header = sc.parallelize([set1header])
 set1 = set1.subtract(header)
 
 def aggregate(x):
-    row = x.split(',')
-    return ( (row[3].lower(),row[1]),  int(row[4]) ) 
+    row = x.split(',') # turns string into list split by commas
+    return ( (row[3].lower(),row[1]),  int(row[4]) ) #return only ((name, gender), count)
 
-set2 = set1.map(aggregate)
+set2 = set1.map(aggregate) 
 
-set3 = set2.reduceByKey(add)
+set3 = set2.reduceByKey(add) 
 
-set3.collect()
+set3.collect() # Final result, with ((name, gender), count)
 
 def maximum(a,b):
     return max(a[1], b[1])
@@ -47,9 +49,38 @@ def malecheck(a):
     else:
         return False
 
-males = set3.filter(malecheck)
+boys = set3.filter(malecheck)
 
-males.collect()
+popularBoy = boys.max(lambda x:x[1])
+popularBoy
+
+def femalecheck(a):
+    if a[0][1] == 'FEMALE':
+        return True
+    else:
+        return False
+
+girls = set3.filter(femalecheck)
+
+popularGirl = girls.max(lambda x:x[1])
+popularGirl
+
+def total(x,y):
+    return (('total'), x[1] + y[1])
+
+totalboys = boys.reduce(total)
+percOfJacobs = 100*popularBoy[1]/totalboys[1]
+
+totalgirls = girls.reduce(total)
+percOfEmmas = 100*popularGirl[1]/totalgirls[1]
+
+def nameMap(x):
+    return (x[0][0], 1)
+
+
+namesOnly = set3.map(nameMap)
+
+genderNeutral = namesOnly.reduceByKey(add)
 
 
 
