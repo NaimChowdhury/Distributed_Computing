1b3588134c3bcae54055c33a62c59e1de5bbe632
NaeemCho <naimul.chowdhury15@ncf.edu>
2020-02-26 14:33:00 -0500

    Produced an RDD that has (name, gender) as key, and count summed as values

--- 2020-02-26_HW2/2020-02-26_HW2.py
+++ 2020-02-26_HW2/2020-02-26_HW2.py
@@ -2,7 +2,7 @@ import random as rand
 
 
 ## Problem A
-useless = sc.parallelize(range(100000))
+useless = sc.parallelize(range(1000000))
 
 def mapping(x):
     row = [rand.randint(1,6) for i in range(1,8)]
@@ -19,4 +19,24 @@ useful = useless.map(mapping)
 count = useful.reduceByKey(add)
 
 probability = count.collect()[1][1] / (count.collect()[0][1] + count.collect()[1][1])
+
+# Problem B
+set1 = sc.textFile("/usr/examples/Popular_Baby_Names.csv")
+
+set1header = set1.first()
+header = sc.parallelize([set1header])
+
+set1 = set1.subtract(header)
+
+def aggregate(x):
+    row = x.split(',')
+    return ( (row[3].lower(),row[1]),  int(row[4]) ) 
+
+set2 = set1.map(aggregate)
+
+set3 = set2.reduceByKey(add)
+
+set3.collect()
+
+
 
